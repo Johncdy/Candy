4ea7a65fe5a4b2a8a29e0fe470a3155deacd2987
Dongyang.Cai <yangguangzaidongji@hotmail.com>
2016-11-28 14:22:55 +0800

    Add scene and modify transform matrix.

---
+++ Candy/object/Scene.cpp
@@ -0,0 +1,9 @@
+//
+//  Scene.cpp
+//  Candy
+//
+//  Created by Dongyang.Cai on 25/11/2016.
+//
+//
+
+#include "Scene.hpp"
---
+++ Candy/object/Scene.h
@@ -0,0 +1,14 @@
+//
+//  Scene.h
+//  Candy
+//
+//  Created by Dongyang.Cai on 25/11/2016.
+//
+//
+
+#ifndef Scene_h
+#define Scene_h
+
+#include <stdio.h>
+
+#endif /* Scene_h */
