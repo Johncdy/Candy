f5aaa5e5316f5b46dd91b1cf2e8502aefe63d6c9
Dongyang.Cai <yangguangzaidongji@hotmail.com>
2017-02-07 20:30:49 +0800

    Add opengl shader vertex and fragment.

--- Candy/renderer/GLProgram.cpp
+++ Candy/renderer/GLProgram.cpp
@@ -8,9 +8,50 @@
 
 #include "renderer/GLProgram.h"
 
+#include <string.h>
+
 NS_DY_BEGIN
 
 NS_RENDERER_BEGIN
+
+/*********************************/
+// Built Shader types
+/*********************************/
+const char* GLProgram::SHADER_NAME_ETC1AS_POSITION_TEXTURE_COLOR = "#ShaderETC1ASPositionTextureColor";
+const char* GLProgram::SHADER_NAME_ETC1AS_POSITION_TEXTURE_COLOR_NO_MVP = "#ShaderETC1ASPositionTextureColor_noMVP";
+
+const char* GLProgram::SHADER_NAME_ETC1AS_POSITION_TEXTURE_GRAY = "#ShaderETC1ASPositionTextureGray";
+const char* GLProgram::SHADER_NAME_ETC1AS_POSITION_TEXTURE_GRAY_NO_MVP = "#ShaderETC1ASPositionTextureGray_noMVP";
+
+const char* GLProgram::SHADER_NAME_POSITION_TEXTURE_COLOR = "ShaderPositionTextureColor";
+const char* GLProgram::SHADER_NAME_POSITION_TEXTURE_COLOR_NO_MVP = "ShaderPositionTextureColor_noMVP";
+const char* GLProgram::SHADER_NAME_POSITION_TEXTURE_ALPHA_TEST = "ShaderPositionTextureColorAlphaTest";
+const char* GLProgram::SHADER_NAME_POSITION_TEXTURE_ALPHA_TEST_NO_MV = "ShaderPositionTextureColorAlphaTest_NoMV";
+const char* GLProgram::SHADER_NAME_POSITION_COLOR = "ShaderPositionColor";
+const char* GLProgram::SHADER_NAME_POSITION_COLOR_TEXASPOINTSIZE = "ShaderPositionColorTexAsPointsize";
+const char* GLProgram::SHADER_NAME_POSITION_COLOR_NO_MVP = "ShaderPositionColor_noMVP";
+
+const char* GLProgram::SHADER_NAME_POSITION_TEXTURE = "ShaderPositionTexture";
+const char* GLProgram::SHADER_NAME_POSITION_TEXTURE_U_COLOR = "ShaderPositionTexture_uColor";
+const char* GLProgram::SHADER_NAME_POSITION_TEXTURE_A8_COLOR = "ShaderPositionTextureA8Color";
+const char* GLProgram::SHADER_NAME_POSITION_U_COLOR = "ShaderPosition_uColor";
+const char* GLProgram::SHADER_NAME_POSITION_LENGTH_TEXTURE_COLOR = "ShaderPositionLengthTextureColor";
+const char* GLProgram::SHADER_NAME_POSITION_GRAYSCALE = "ShaderUIGrayScale";
+
+/*********************************/
+// Built Attribute names
+/*********************************/
+const char* GLProgram::ATTRIBUTE_NAME_COLOR = "a_color";
+const char* GLProgram::ATTRIBUTE_NAME_POSITION = "a_position";
+const char* GLProgram::ATTRIBUTE_NAME_TEX_COORD0 = "a_texCoord";
+const char* GLProgram::ATTRIBUTE_NAME_TEX_COORD1 = "a_texCoord1";
+const char* GLProgram::ATTRIBUTE_NAME_TEX_COORD2 = "a_texCoord2";
+const char* GLProgram::ATTRIBUTE_NAME_TEX_COORD3 = "a_texCoord3";
+const char* GLProgram::ATTRIBUTE_NAME_NORMAL = "a_normal";
+const char* GLProgram::ATTRIBUTE_NAME_BLEND_WEIGHT = "a_blendWeight";
+const char* GLProgram::ATTRIBUTE_NAME_BLEND_INDEX = "a_blendIndex";
+const char* GLProgram::ATTRIBUTE_NAME_TANGENT = "a_tangent";
+const char* GLProgram::ATTRIBUTE_NAME_BINORMAL = "a_binormal";
 
 static const char* DY_SHADER_UNIFORMS =
     "uniform mat4 DY_PMatrix;\n"
@@ -48,6 +89,7 @@ GLProgram::GLProgram()
 , _vertShader(0)
 , _fragShader(0)
 {
+    memset(_builtInUniforms, 0, sizeof(_builtInUniforms));
 }
 
 GLProgram::~GLProgram()
@@ -58,6 +100,8 @@ GLProgram* GLProgram::create(const char *vertByteArray, const char *fragByteArra
 {
     GLProgram* ret = new (std::nothrow)GLProgram();
     if (ret && ret->init(vertByteArray, fragByteArray)) {
+        ret->link();
+        
         ret->autoRelease();
         return ret;
     }
@@ -99,19 +143,6 @@ bool GLProgram::init(const char *vertByteArray, const char *fragByteArray)
     return true;
 }
 
-bool GLProgram::link()
-{
-    DY_ASSERT(_program && "Candy: Cannot link invalid program");
-    
-    GLint status = GL_TRUE:
-    
-    return true;
-}
-
-void GLProgram::use()
-{
-}
-
 bool GLProgram::compileShader(GLuint* shader, GLenum type, const GLchar* source)
 {
     GLint status;
@@ -150,6 +181,157 @@ bool GLProgram::compileShader(GLuint* shader, GLenum type, const GLchar* source)
     }
     
     return (status == GL_TRUE);
+}
+
+bool GLProgram::link()
+{
+    DY_ASSERT(_program && "Candy: Cannot link invalid program");
+    
+    GLint status = GL_TRUE;
+    
+    bindPredefinedVertexAttribs();
+    
+    glLinkProgram(_program);
+    
+    // Calling glGetProgramiv(...GL_LINK_STATUS...) will force linking of the program at this moment.
+    glGetProgramiv(_program, GL_LINK_STATUS, &status);
+    
+    if (GL_FALSE == status) {
+        printf("Candy: Failed to link program: %i", _program);
+        glDeleteProgram(_program);
+        _program = 0;
+    } else {
+        parseVertexAttribs();
+        parseUniforms();
+        
+        clearShader();
+    }
+    
+    return (status == GL_TRUE);
+}
+
+void GLProgram::bindPredefinedVertexAttribs()
+{
+    // Bind predefined vertexAttribs.
+    struct attributeLocation {
+        const char* attributeName;
+        int location;
+    };
+    attributeLocation locations[] = {
+        {ATTRIBUTE_NAME_POSITION,   VERTEX_ATTRIB_POSITION},
+        {ATTRIBUTE_NAME_COLOR,      VERTEX_ATTRIB_COLOR},
+        {ATTRIBUTE_NAME_TEX_COORD0, VERTEX_ATTRIB_TEX_COORD0},
+        {ATTRIBUTE_NAME_TEX_COORD1, VERTEX_ATTRIB_TEX_COORD1},
+        {ATTRIBUTE_NAME_TEX_COORD2, VERTEX_ATTRIB_TEX_COORD2},
+        {ATTRIBUTE_NAME_TEX_COORD3, VERTEX_ATTRIB_TEX_COORD3},
+        {ATTRIBUTE_NAME_NORMAL,     VERTEX_ATTRIB_NORMAL},
+    };
+    
+    const int size = sizeof(locations) / sizeof(locations[0]);
+    
+    for (int i = 0; i < size; i++) {
+        glBindAttribLocation(_program, locations[i].location, locations[i].attributeName);
+    }
+}
+
+void GLProgram::parseVertexAttribs()
+{
+    // Query and store vertex attribute meta-data from the program.
+    GLint activeAttributes;
+    GLint length;
+    glGetProgramiv(_program, GL_ACTIVE_ATTRIBUTES, &activeAttributes);
+    if (activeAttributes > 0) {
+        VertexAttrib attribute;
+        
+        glGetProgramiv(_program, GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, &length);
+        if (length > 0) {
+            GLchar* attribName = (GLchar*)alloca(length + 1);
+            for (int i = 0; i < activeAttributes; i++) {
+                // Query attribute info.
+                glGetActiveAttrib(_program, i, length, nullptr, &attribute._size, &attribute._type, attribName);
+                attribName[length] = '\0';
+                attribute._name = std::string(attribName);
+                
+                // Query the pre-assigned attribute location
+                attribute._index = glGetAttribLocation(_program, attribName);
+                
+                _vertexAttribs[attribute._name] = attribute;
+            }
+        }
+    } else {
+        GLchar errorLog[1024];
+        glGetProgramInfoLog(_program, sizeof(errorLog), nullptr, errorLog);
+        printf("Candy: Error linking shader program: %s\n", errorLog);
+    }
+}
+
+void GLProgram::parseUniforms()
+{
+    // Query and store uniforms from the program.
+    GLint activeUniforms;
+    GLint length;
+    glGetProgramiv(_program, GL_ACTIVE_UNIFORMS, &activeUniforms);
+    if (activeUniforms > 0) {
+        Uniform uniform;
+        
+        glGetProgramiv(_program, GL_ACTIVE_UNIFORM_MAX_LENGTH, &length);
+        if (length > 0) {
+            GLchar* uniformName = (GLchar*)alloca(length + 1);
+            for (int i = 0; i < activeUniforms; i++) {
+                // Query uniform info.
+                glGetActiveUniform(_program, i, length, nullptr, &uniform._size, &uniform._type, uniformName);
+                uniformName[length] = '\0';
+                uniform._name = uniformName;
+                
+                // Only add uniforms that are not built-in.
+                // The ones that start with 'DY_' are built-ins
+                if (strncmp("DY_", uniformName, 3) != 0) {
+                    // remove possible array '[]' from uniform name
+                    if (length > 3) {
+                        char* c = strrchr(uniformName, '[');
+                        if (c) {
+                            *c = '\0';
+                        }
+                    }
+                    uniform._location = glGetUniformLocation(_program, uniformName);
+                    uniform._name = std::string(uniformName);
+                    GLenum __gl_error_code = glGetError();
+                    if (__gl_error_code != GL_NO_ERROR)
+                    {
+                        printf("error: 0x%x  uniformName: %s", (int)__gl_error_code, uniformName);
+                    }
+                    DY_ASSERT(__gl_error_code == GL_NO_ERROR);
+                    
+                    _uniforms[uniform._name] = uniform;
+                }
+            }
+        }
+    } else {
+        GLchar errorLog[1024];
+        glGetProgramInfoLog(_program, sizeof(errorLog), nullptr, errorLog);
+        printf("Candy: Error linking shader program: %s\n", errorLog);
+    }
+}
+
+void GLProgram::clearShader()
+{
+    if (_vertShader) {
+        glDeleteShader(_vertShader);
+    }
+    
+    if (_fragShader) {
+        glDeleteShader(_fragShader);
+    }
+    
+    _vertShader = _fragShader = 0;
+}
+
+void GLProgram::updateUniforms()
+{
+}
+
+void GLProgram::use()
+{
 }
 
 NS_RENDERER_END
--- Candy/renderer/GLProgram.h
+++ Candy/renderer/GLProgram.h
@@ -35,17 +35,20 @@ struct Uniform {
 // VertexAttrib is a structure to encapsulate data got from glGetActiveAttrib.
 struct VertexAttrib {
     // Index of attribute, start from 0.
-    GLuint index;
+    GLuint _index;
     // Number of Data type in the attribute, could range from 0-4.
-    GLint size;
+    GLint _size;
     // Data type of the attribute, could be GL_FLOAT, GL_UNSIGNED_BYTE etc.
-    GLenum type;
+    GLenum _type;
     // The string name in vertex shader.
-    std::string name;
+    std::string _name;
 };
 
 class GLProgram : public object::Ref {
 public:
+    /*********************************/
+    // Built Shader types
+    /*********************************/
     // ETC1 ALPHA supports for 2d.
     static const char* SHADER_NAME_ETC1AS_POSITION_TEXTURE_COLOR;
     static const char* SHADER_NAME_ETC1AS_POSITION_TEXTURE_COLOR_NO_MVP;
@@ -77,9 +80,136 @@ public:
     static const char* SHADER_NAME_POSITION_U_COLOR;
     // Built in shader for draw a sector with 90 degrees with center at bottom left point.
     static const char* SHADER_NAME_POSITION_LENGTH_TEXTURE_COLOR;
-    
     // Built in shader for ui effects.
     static const char* SHADER_NAME_POSITION_GRAYSCALE;
+    
+    /*********************************/
+    // Built Uniform names.
+    /*********************************/
+    // Ambient Color uniform.
+    static const char* UNIFORM_NAME_AMBIENT_COLOR;
+    // Projection Matrix uniform.
+    static const char* UNIFORM_NAME_P_MATRIX;
+    // Model view matrix uniform.
+    static const char* UNIFORM_NAME_MV_MATRIX;
+    // Model view projection uniform.
+    static const char* UNIFORM_NAME_MVP_MATRIX;
+    // Normal matrix uniform.
+    static const char* UNIFORM_NAME_NORMAL_MATRIX;
+    // Time uniform.
+    static const char* UNIFORM_NAME_TIME;
+    // Sin time uniform.
+    static const char* UNIFORM_NAME_SIN_TIME;
+    // Cos time uniform.
+    static const char* UNIFORM_NAME_COS_TIME;
+    // Random number uniform.
+    static const char* UNIFORM_NAME_RANDOM01;
+    
+    // Sampler uniform 0-3, used for textures.
+    static const char* UNIFORM_NAME_SAMPLER0;
+    static const char* UNIFORM_NAME_SAMPLER1;
+    static const char* UNIFORM_NAME_SAMPLER2;
+    static const char* UNIFORM_NAME_SAMPLER3;
+    
+    // Alpha test value uniform.
+    static const char* UNIFORM_NAME_ALPHA_TEST_VALUE;
+    
+    // Preallocated uniform handle.
+    enum
+    {
+        // Ambient color.
+        UNIFORM_AMBIENT_COLOR,
+        // Projection matrix.
+        UNIFORM_P_MATRIX,
+        // Model view matrix.
+        UNIFORM_MV_MATRIX,
+        // Model view projection matrix.
+        UNIFORM_MVP_MATRIX,
+        // Normal matrix.
+        UNIFORM_NORMAL_MATRIX,
+        // Time.
+        UNIFORM_TIME,
+        // sin(Time).
+        UNIFORM_SIN_TIME,
+        // cos(Time).
+        UNIFORM_COS_TIME,
+        // Random number.
+        UNIFORM_RANDOM01,
+        
+        // Sampler 0-3, used for texture.
+        UNIFORM_SAMPLER0,
+        UNIFORM_SAMPLER1,
+        UNIFORM_SAMPLER2,
+        UNIFORM_SAMPLER3,
+        
+        // Index length.
+        UNIFORM_MAX,
+    };
+    
+    /*********************************/
+    // Built Attribute names
+    /*********************************/
+    // Attribute color.
+    static const char* ATTRIBUTE_NAME_COLOR;
+    // Attribute position.
+    static const char* ATTRIBUTE_NAME_POSITION;
+    // Attribute Texcoord 0-3.
+    static const char* ATTRIBUTE_NAME_TEX_COORD0;
+    static const char* ATTRIBUTE_NAME_TEX_COORD1;
+    static const char* ATTRIBUTE_NAME_TEX_COORD2;
+    static const char* ATTRIBUTE_NAME_TEX_COORD3;
+    // Attribute normal.
+    static const char* ATTRIBUTE_NAME_NORMAL;
+    // Attribute blend weight.
+    static const char* ATTRIBUTE_NAME_BLEND_WEIGHT;
+    // Attribute blend index.
+    static const char* ATTRIBUTE_NAME_BLEND_INDEX;
+    // Attribute blend tangent.
+    static const char* ATTRIBUTE_NAME_TANGENT;
+    // Attribute blend binormal.
+    static const char* ATTRIBUTE_NAME_BINORMAL;
+    
+    // Enum the preallocated vertex attribute.
+    enum
+    {
+        // Index 0 will be used as Position.
+        VERTEX_ATTRIB_POSITION,
+        // Index 1 will be used as Color.
+        VERTEX_ATTRIB_COLOR,
+        // Index 2 will be used as Tex coord unit 0.
+        VERTEX_ATTRIB_TEX_COORD0,
+        // Index 3 will be used as Tex coord unit 1.
+        VERTEX_ATTRIB_TEX_COORD1,
+        // Index 4 will be used as Tex coord unit 2.
+        VERTEX_ATTRIB_TEX_COORD2,
+        // Index 5 will be used as Tex coord unit 3.
+        VERTEX_ATTRIB_TEX_COORD3,
+        // Index 6 will be used as Normal.
+        VERTEX_ATTRIB_NORMAL,
+        // Index 7 will be used as Blend weight for hardware skin.
+        VERTEX_ATTRIB_BLEND_WEIGHT,
+        // Index 8 will be used as Blend index.
+        VERTEX_ATTRIB_BLEND_INDEX,
+        // Index 9 will be used as Tangent.
+        VERTEX_ATTRIB_TANGENT,
+        // Index 10 will be used as Binormal.
+        VERTEX_ATTRIB_BINORMAL,
+        VERTEX_ATTRIB_MAX,
+        
+        VERTEX_ATTRIB_TEX_COORDS = VERTEX_ATTRIB_TEX_COORD0,
+    };
+    
+    // Flags used by the uniforms.
+    struct UniformFlags {
+        unsigned int usesTime:1;
+        unsigned int usesNormal:1;
+        unsigned int usesMVP:1;
+        unsigned int usesMV:1;
+        unsigned int usesP:1;
+        unsigned int usesRandom:1;
+        // handy way to initialize the bitfield
+        UniformFlags() { memset(this, 0, sizeof(*this)); }
+    };
     
     DY_CONSTRUCTOR_FUNC(GLProgram);
     
@@ -95,18 +225,6 @@ public:
     bool init(const GLchar* vertByteArray, const GLchar* fragByteArray);
     
     /**
-     links the glProgram
-
-     @return link result
-     */
-    bool link();
-    
-    /**
-     it will call glUseProgram()
-     */
-    void use();
-    
-    /**
      Compile the shader sources.
 
      @param shader shader opengl handle
@@ -116,6 +234,49 @@ public:
      */
     bool compileShader(GLuint* shader, GLenum type, const GLchar* source);
     
+    /**
+     links the glProgram
+     
+     @return link result
+     */
+    bool link();
+    
+    /**
+     Bind the predefined vertex attributes to their specific slot.
+     */
+    void bindPredefinedVertexAttribs();
+    
+    /**
+     Parse user defined Vertex Attributes automatically.
+     */
+    void parseVertexAttribs();
+    
+    /**
+     Parse user defined uniform automatically.
+     */
+    void parseUniforms();
+    
+    /**
+     Clear vertex and fragment shader.
+     */
+    void clearShader();
+    
+    /**
+     It will create 4 uniforms:
+     - kUniformPMatrix
+     - kUniformMVMatrix
+     - kUniformMVPMatrix
+     - GLProgram::UNIFORM_SAMPLER
+     
+     And it will bind "GLProgram::UNIFORM_SAMPLER" to 0
+     */
+    void updateUniforms();
+    
+    /**
+     it will call glUseProgram()
+     */
+    void use();
+    
 protected:
     // OpenGL handle for program.
     GLuint _program;
@@ -123,6 +284,8 @@ protected:
     GLuint _vertShader;
     // OpenGL handle for fragment shader.
     GLuint _fragShader;
+    // Built in uniforms.
+    GLint _builtInUniforms[UNIFORM_MAX];
     
     // User defined Uniforms.
     std::unordered_map<std::string, Uniform> _uniforms;
